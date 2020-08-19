47580a2f8382c17cde68aa3bef3b637b9c414723
Kate <kate1996970@gmail.com>
2020-08-19 17:12:46 +0300

    change js

--- Ishop/MyIshop.js
+++ Ishop/MyIshop.js
@@ -9,10 +9,10 @@ var MyIshop = React.createClass({
         var answer=this.props.answers[a];
         var answerCode=        
           React.DOM.div({key:answer.code,className:'Answer'},
-            React.DOM.span({className:'Count'},answer.count),
             React.DOM.span({className:'Text'},answer.text),
             React.DOM.span({className:'Price'},answer.price),
             React.DOM.span({className:'Url'},answer.url),
+            React.DOM.span({className:'Count'},answer.count),
           );
         answersCode.push(answerCode);
       }
