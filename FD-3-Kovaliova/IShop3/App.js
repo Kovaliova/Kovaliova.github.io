fcb43d8eb36ac831a9745d010ba1fcb013f00a40
Kovaliova <43334165+Kovaliova@users.noreply.github.com>
2020-09-20 15:55:22 +0300

    add

---
+++ FD-3-Kovaliova/IShop3/App.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ListComponent from './components/ListComponent/ListComponent';
+
+var tableName = 'Catalog AppleService';
+var tableHeaders = require('./data/theadtable.json');
+var itemList = require('./data/items.json');
+var question = 'Do you want to delete this row?';
+
+ReactDOM.render(
+  <ListComponent
+    tableName={tableName}
+    tableHeaders={tableHeaders}
+    itemList={itemList}
+    question={question}
+  />, document.getElementById('container')
+)
