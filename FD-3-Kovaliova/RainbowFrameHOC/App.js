34d2ae22d99a12f618ba6d3d5127dee881f1a3bb
Kovaliova <43334165+Kovaliova@users.noreply.github.com>
2020-09-27 12:47:40 +0300

    add

---
+++ FD-3-Kovaliova/RainbowFrameHOC/App.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrameHOC   from './components/RainbowFrameHOC';
+import './app.css';
+
+ReactDOM.render( 
+  <RainbowFrameHOC  />, document.getElementById('container')
+);
