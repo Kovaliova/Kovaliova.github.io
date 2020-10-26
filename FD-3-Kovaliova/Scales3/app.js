933e78a3bf75ce73a32faee9f221849a8b2953fa
Kate <kate1996970@gmail.com>
2020-10-26 19:37:39 +0300

    add

---
+++ FD-3-Kovaliova/Scales3/app.js
@@ -0,0 +1,99 @@
+var Scales = /** @class */ (function () {
+    function Scales(_storage) {
+        //this.productsArr=[]; 
+        this.storage = _storage;
+    }
+    Scales.prototype.add = function (_product) {
+        this.storage.addItem(_product);
+    };
+    Scales.prototype.getSumScale = function () {
+        var _sumScale = 0;
+        var quantity = this.storage.getCount();
+        for (var i = 0; i < quantity; i++) {
+            _sumScale += this.storage.getItem(i).getScale();
+        }
+        return _sumScale;
+    };
+    Scales.prototype.getNameList = function () {
+        var _productsList = [];
+        var quantity = this.storage.getCount();
+        for (var i = 0; i < quantity; i++) {
+            _productsList.push(this.storage.getItem(i).getName());
+        }
+        return _productsList;
+    };
+    return Scales;
+}());
+var Product = /** @class */ (function () {
+    function Product(_name, _weight) {
+        this.name = _name;
+        this.weight = _weight;
+        Product.numOfProducts++;
+    }
+    Product.prototype.getScale = function () {
+        return this.weight;
+    };
+    Product.prototype.getName = function () {
+        return this.name;
+    };
+    Product.numOfProducts = 0; //кол-во созданных продуктов
+    return Product;
+}());
+var ScalesStorageEngineArray = /** @class */ (function () {
+    function ScalesStorageEngineArray() {
+        this.storage = [];
+    }
+    ScalesStorageEngineArray.prototype.addItem = function (_item) {
+        this.storage.push(_item);
+    };
+    ;
+    ScalesStorageEngineArray.prototype.getItem = function (_index) {
+        return this.storage[_index];
+    };
+    ;
+    ScalesStorageEngineArray.prototype.getCount = function () {
+        return this.storage.length;
+    };
+    ;
+    return ScalesStorageEngineArray;
+}());
+var ScalesStorageEngineLocalStorage = /** @class */ (function () {
+    function ScalesStorageEngineLocalStorage() {
+        this.key = 'products'; //ключ, под которым хранится информация
+        if (localStorage.products) {
+            this.storage = JSON.parse(localStorage.products);
+        }
+        else {
+            this.storage = [];
+        }
+    }
+    ScalesStorageEngineLocalStorage.prototype.addItem = function (item) {
+        this.storage.push(item);
+        localStorage.products = JSON.stringify(this.storage);
+    };
+    ScalesStorageEngineLocalStorage.prototype.getItem = function (_index) {
+        //let a:any =JSON.parse(localStorage.products);
+        return new Product(this.storage[_index].name, this.storage[_index].weight);
+    };
+    ScalesStorageEngineLocalStorage.prototype.getCount = function () {
+        //let a: any = JSON.parse(localStorage.products);
+        return this.storage.length;
+    };
+    return ScalesStorageEngineLocalStorage;
+}());
+localStorage.clear();
+var scales1 = new Scales(new ScalesStorageEngineArray());
+var scales2 = new Scales(new ScalesStorageEngineLocalStorage());
+var product1 = new Product("AnisApple", 150);
+var product2 = new Product("BlackTomato", 200);
+var product3 = new Product("GoldenApple", 160);
+var product4 = new Product("YellowTomato", 190);
+var product5 = new Product("VictoryPear", 100);
+scales1.add(product1);
+scales1.add(product2);
+scales2.add(product3);
+scales2.add(product4);
+scales2.add(product5);
+console.log(scales1.getSumScale(), scales1.getNameList());
+console.log(scales2.getSumScale(), scales2.getNameList());
+//# sourceMappingURL=app.js.map
---
+++ FD-3-Kovaliova/Scales3/app.ts
@@ -0,0 +1,141 @@
+//интерфейс механизма хранения
+interface IStorageEngine {
+    addItem(item:Product):void;
+    getItem(index:number):Product;
+    getCount():number;
+}
+
+class Scales<StorageEngine  extends IStorageEngine> {
+    
+    //productsArr:Array<Product>;
+    storage:StorageEngine;
+
+    constructor(_storage:StorageEngine) {
+        //this.productsArr=[]; 
+        this.storage=_storage;
+    }
+
+    add(_product:Product):void {
+        this.storage.addItem(_product);
+    }
+
+    getSumScale():number {
+        let _sumScale:number = 0;
+        
+        let quantity:number=this.storage.getCount();
+        for(let i:number=0; i<quantity; i++){
+            _sumScale += this.storage.getItem(i).getScale();
+        }               
+        return _sumScale;
+    }
+
+    getNameList():Array<string> {
+        let _productsList:Array<string> = [];
+        let quantity:number=this.storage.getCount();
+        for(let i:number=0; i<quantity; i++){
+            _productsList.push(this.storage.getItem(i).getName());
+        }   
+        
+        return _productsList;
+    }
+}
+
+class Product {
+
+    name:string;
+    weight:number;
+
+    static numOfProducts:number = 0;//кол-во созданных продуктов
+
+    constructor(_name:string, _weight:number) {
+        this.name=_name;
+        this.weight=_weight;
+        Product.numOfProducts++;
+    }
+
+    public getScale():number {   //получить вес продукта
+        return this.weight;
+    }
+
+    public getName():string {   //получить название продукта
+        return this.name;
+    }
+    
+}
+
+
+class ScalesStorageEngineArray implements IStorageEngine {
+    
+    storage:Array<Product> = [];
+
+    addItem(_item:Product):void {
+        this.storage.push(_item);
+    };
+
+    getItem(_index:number):Product {
+        return this.storage[_index];
+    };
+
+    getCount():number {
+        return this.storage.length;
+    };
+
+}    
+
+class ScalesStorageEngineLocalStorage implements IStorageEngine {
+     
+    storage:Array<Product>; //хранящийся массив
+    key:string = 'products'; //ключ, под которым хранится информация
+
+    constructor() {
+       
+        if(localStorage.products){
+            this.storage=JSON.parse(localStorage.products);
+        }else {
+            this.storage=[];
+        }
+    }
+
+    addItem(item:Product):void {
+       
+        this.storage.push(item);
+        localStorage.products = JSON.stringify(this.storage); 
+     }
+
+    getItem(_index:number):Product {
+        //let a:any =JSON.parse(localStorage.products);
+        return new Product(this.storage[_index].name,this.storage[_index].weight);
+        
+
+     }    
+
+    getCount():number {
+        //let a: any = JSON.parse(localStorage.products);
+        return this.storage.length;
+    }
+
+
+}
+
+localStorage.clear();
+
+let scales1:Scales<ScalesStorageEngineArray> = new Scales(new ScalesStorageEngineArray());
+let scales2:Scales<ScalesStorageEngineLocalStorage> = new Scales(new ScalesStorageEngineLocalStorage());
+
+let product1:Product = new Product("AnisApple", 150);
+let product2:Product = new Product("BlackTomato", 200);
+
+let product3:Product = new Product("GoldenApple", 160);
+let product4:Product = new Product("YellowTomato", 190);
+let product5:Product = new Product("VictoryPear", 100);
+
+scales1.add(product1);
+scales1.add(product2);
+
+scales2.add(product3);
+scales2.add(product4);
+scales2.add(product5);
+
+
+console.log(scales1.getSumScale(), scales1.getNameList());
+console.log(scales2.getSumScale(), scales2.getNameList());
